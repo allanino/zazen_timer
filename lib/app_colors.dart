7df08a6abb1b4d86aeaf7e361f0870112e1560b9
Allan Costa <allaninocencio@yahoo.com.br>
2026-02-26 10:36:41 -0300

    Adjust kAccentColor (blueish)

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const Color kAccentColor =  Color.fromARGB(255, 179, 186, 193); // Color(0xFF7FA2B8);
+const Color kAccentColor =  Color.fromARGB(255, 193, 219, 248); // Color(0xFF7FA2B8);
 
